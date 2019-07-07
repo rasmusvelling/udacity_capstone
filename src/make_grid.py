41c5d092ddb48ab5e7abe3ebbe56616bdd0faad5
rasmusvelling <rasmusveling>
2019-07-07 08:18:54 +0200

    Added SVC lin

--- src/make_grid.py
+++ src/make_grid.py
@@ -69,12 +69,12 @@ def make_grid():
     }
     models.append(pd.DataFrame(src.expand_grid(**models_tmp)))
 
-    # # SVC lin
-    # models_tmp = {
-    #     'model_framework': ['mod_svc_lin'],
-    #     'C': [1]
-    # }
-    # models.append(pd.DataFrame(src.expand_grid(**models_tmp)))
+    # SVC lin
+    models_tmp = {
+        'model_framework': ['mod_svc_lin'],
+        'C': [1]
+    }
+    models.append(pd.DataFrame(src.expand_grid(**models_tmp)))
 
     # # SVC poly
     # models_tmp = {
